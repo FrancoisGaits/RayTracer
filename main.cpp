b49cc9451169eda69a119483b35110458f6bb4cc
Francois_Gaits <45246324+FrancoisGaits@users.noreply.github.com>
2019-02-19 22:14:06 +0100

    rotate cam

--- main.cpp
+++ main.cpp
@@ -275,76 +275,40 @@ Scene *initScene4() {
 
 Scene *initScene42(int i, int nb_step) {
   Scene *scene = initScene();
-  double coeff = (nb_step-i)/(double)nb_step;
+  double coeff = 1. - (nb_step-i)/(double)nb_step;
   // setCamera(scene, point3( 4.5 + (10.f*coeff) , .8 + (8.f*coeff), 4.5 + (2.f*coeff)), vec3(0, 0.3, 0), vec3(0, 1, 0), 60,
   //           (float)WIDTH / (float)HEIGHT);
 
-  setCamera(scene,point3(rotateZ(vec3(0, 1, 0) - vec3(4.5,.8,4.5), glm::pi<float>()*float(coeff)*0.5f)),vec3(0, 0.3, 0), vec3(0, 1, 0), 60,(float)WIDTH / (float)HEIGHT);
+  setCamera(scene,point3(rotateZ(vec3(0,4,2.5) - vec3(0, 0, 0.6), glm::pi<float>()*float(coeff)*2.f)),vec3(0, 0, 0.6), vec3(0, 0, 1), 60,(float)WIDTH / (float)HEIGHT);
+
   setSkyColor(scene, color3(0.2, 0.2, 0.7));
   Material mat;
-  mat.diffuseColor = color3(0.301, 0.034, 0.039);
-  mat.specularColor = color3(1.0, 0.992, 0.98);
-  mat.IOR = 1.1382;
-  mat.roughness = 0.0886;
-
-  addLight(scene, initLight(point3(0, 1.7, 1), .5f * color3(3, 3, 3)));
-  addLight(scene, initLight(point3(3, 2, 3), .5f * color3(4, 4, 4)));
-  addLight(scene, initLight(point3(4, 3, -1), .5f * color3(5, 5, 5)));
-
   mat.diffuseColor = color3(0.014, 0.012, 0.012);
-  mat.specularColor = color3(0.7, 0.882, 0.786);
-  mat.IOR = 6;
-  mat.roughness = 0.0181;
-  addObject(scene, initSphere(point3(0, 0.1, 0), .3, mat));
+  mat.specularColor = color3(1.0, 0.882, 0.786);
+  mat.IOR = 2.4449;
+  mat.roughness = 0.0681;
 
-  mat.diffuseColor = color3(0.26, 0.036, 0.014);
-  mat.specularColor = color3(1.0, 0.852, 1.172);
-  mat.IOR = 1.3771;
-  mat.roughness = 0.01589;
-  addObject(scene, initSphere(point3(1, -.05, 0), .15, mat));
+  mat.diffuseColor = color3(0.05, 0.05, 0.05);
+  mat.specularColor = color3(0.95);
+  mat.IOR = 1.1022;
+  mat.roughness = 0.0579;
 
-  mat.diffuseColor = color3(0.014, 0.012, 0.012);
-  mat.specularColor = color3(0.7, 0.882, 0.786);
-  mat.IOR = 3;
-  mat.roughness = 0.00181;
-  addObject(scene, initSphere(point3(3, 0.05, 2), .25, mat));
+  addObject(scene, initPlane(vec3(0, 0, 1), 0, mat));
 
-  mat.diffuseColor = color3(0.46, 0.136, 0.114);
-  mat.specularColor = color3(0.8, 0.852, 0.8172);
-  mat.IOR = 1.5771;
-  mat.roughness = 0.01589;
-  addObject(scene, initSphere(point3(1.3, 0., 2.6), 0.215, mat));
-
-  mat.diffuseColor = color3(0.06, 0.26, 0.22);
-  mat.specularColor = color3(0.70, 0.739, 0.721);
-  mat.IOR = 1.3051;
-  mat.roughness = 0.567;
-  addObject(scene, initSphere(point3(1.9, 0.05, 2.2), .25, mat));
-
-  mat.diffuseColor = color3(0.012, 0.036, 0.406);
-  mat.specularColor = color3(1.0, 0.965, 1.07);
-  mat.IOR = 1.1153;
-  mat.roughness = 0.068;
-  mat.roughness = 0.18;
-  addObject(scene, initSphere(point3(0, 0, 1), .20, mat));
-
-  mat.diffuseColor = color3(.2, 0.4, .3);
-  mat.specularColor = color3(.2, 0.2, .2);
-  mat.IOR = 1.382;
-  mat.roughness = 0.05886;
-  addObject(scene, initPlane(vec3(0, 1, 0), 0.2, mat));
-
-  mat.diffuseColor = color3(.5, 0.09, .07);
-  mat.specularColor = color3(.2, .2, .1);
-  mat.IOR = 1.8382;
-  mat.roughness = 0.886;
-  addObject(scene, initPlane(vec3(1, 0.0, -1.0), 2, mat));
-
-  mat.diffuseColor = color3(0.1, 0.3, .05);
-  mat.specularColor = color3(.5, .5, .5);
-  mat.IOR = 1.9382;
-  mat.roughness = 0.0886;
-  addObject(scene, initPlane(vec3(0.3, -0.2, 1), 4, mat));
+  mat.diffuseColor = color3(0.005, 0.013, 0.032);
+  mat.specularColor = color3(1.0, 0.748, 0.718);
+  for (int i = 0; i < 4; ++i) {
+    mat.IOR = 1.1051 + (-0.1 + float(i) / 3.f * 0.4);
+    for (int j = 0; j < 10; ++j) {
+      mat.roughness = 0.0568 + (-0.1 + float(j) / 9.f * 0.3);
+      addObject(scene, initSphere(point3(-1.5 + float(j) / 9.f * 3.f, 0,
+                                         0.4 + float(i) * 0.4f),
+                                  .15, mat));
+    }
+  }
+  addLight(scene, initLight(point3(-20, 5, 10), color3(30, 30, 30)));
+  addLight(scene, initLight(point3(10, 10, 10), color3(30, 30, 30)));
+  addLight(scene, initLight(point3(50, -100, 10), color3(1, 0.7, 2)));
   return scene;
 }
 
@@ -393,7 +357,7 @@ int main(int argc, char *argv[]) {
     break;
   }
 
-  int nb_step = 5;
+  int nb_step = 200;
   if(scene_id == 42) {
     for(int i=0;i<nb_step;++i) {
       scene = initScene42(i,nb_step);
